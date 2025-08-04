8e6687f2ac34f2c01881206bbc3156a0c54c3281
Bomy Rhee <50459262+20201141@users.noreply.github.com>
2025-08-03 22:29:27 -0400

    convert header to dashboard as component

---
+++ frontend/src/components/WebHeader.tsx
@@ -0,0 +1,19 @@
+import { Separator } from "@/components/ui/separator"
+import { SidebarTrigger } from "@/components/ui/sidebar";
+
+export function WebHeader() {
+  return (
+    <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+          <div className="flex items-center gap-2 px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator
+              orientation="vertical"
+              className="mr-2 data-[orientation=vertical]:h-4"
+            />
+            <h1 className="font-semibold">Dashboard</h1>
+          </div>
+    </header>
+  )
+}
+
+export default WebHeader;
--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,9 +1,8 @@
 import { AppSidebar } from "@/components/AppSidebar"
-import { Separator } from "@/components/ui/separator"
+import { WebHeader } from "@/components/WebHeader"
 import {
   SidebarInset,
   SidebarProvider,
-  SidebarTrigger,
 } from "@/components/ui/sidebar"
 
 export default function Dashboard() {
@@ -11,16 +10,7 @@ export default function Dashboard() {
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
-          <div className="flex items-center gap-2 px-4">
-            <SidebarTrigger className="-ml-1" />
-            <Separator
-              orientation="vertical"
-              className="mr-2 data-[orientation=vertical]:h-4"
-            />
-            <h1 className="font-semibold">Dashboard</h1>
-          </div>
-        </header>
+        <WebHeader />
         {/* Placeholders for main dashboard */}
         <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
           <div className="grid auto-rows-min gap-4 md:grid-cols-3">
