30eb04c890378612ff1e290169bdba04c51b848d
Bomy Rhee <50459262+20201141@users.noreply.github.com>
2025-08-03 23:25:56 -0400

    fix for tailwind css v4

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    darkMode: ["class"],
-    content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+	darkMode: ["class"],
+	content: [
+	"./index.html",
+	"./src/**/*.{js,ts,jsx,tsx}",
+	"./public/**/*.html",
   ],
   theme: {
   	extend: {
